28641b4b1e377a3fa465b482b7dcc3ddea845f32
juansamdphd <jucamilo.sanchez@gmail.com>
2020-04-16 23:17:09 -0700

    Update covid19_col.R

--- covid19_col.R
+++ covid19_col.R
@@ -1,23 +1,27 @@
-library(googlesheets4)
+library(RSocrata)
+library(lubridate)
 library(tidyverse)
 
 # Read Data from INS CO
-sheets_deauth()
+# key name: covid19_col
 
-covid_col <- sheets_read("1l76JEKrN9_2wdREXhL74qVgoppXJBqjP6G8oWxLipTA")
+### API key datos gov.co: datos_col
+### key ID: c3moeeui79vxz3mcp4vqidh72
+### secret key: 77ousfqh4a0ukrnm5ltxifr70z3c9f514z3jau1vv7ds1bdg4
 
-# Changing names to something R compatible
-colnames(covid_col) <- c("ID", "Fecha_dx", "Ciudad", "Dept_Dist", "Status", "Edad", "Sexo", "Tipo", "Origen")
+covid19_col <- read.socrata(
+  "https://www.datos.gov.co/resource/gt2j-8ykr.json",
+  app_token = "gBnYk2R4qxZFqwev2oYi3aYuT",
+  email     = "jucamilo.sanchez@gmail.com",
+  password  = "Sarias23!"
+)
 
-# Converting Excel date format to regular format, Excel Origin is "1899-12-30" in Windows, and "1904-01-01". Not sure if Mac's format translates to UNIX based systems (?) -Not needed anymore
-# covid_col$Fecha_dx <- as.Date(covid_col$Fecha_dx, origin = "1899-12-30")
+covid19_col %>% filter(ciudad_de_ubicaci_n =="CALI") %>% ggplot(aes(x = fecha_de_diagn_stico)) + stat_count(aes(y=cumsum(..count..)))
 
-covid_col %>% filter(Ciudad =="CALI") %>% ggplot(aes(x = Fecha_dx)) + stat_count(aes(y=cumsum(..count..)))
+covid19_col %>% filter(Ciudad == "BOGOTA" | Ciudad == "CALI" | Ciudad == "MEDELLIN") %>% ggplot(aes(x = Fecha_dx, fill = Ciudad)) + stat_count()
 
-covid_col %>% filter(Ciudad == "BOGOTA" | Ciudad == "CALI" | Ciudad == "MEDELLIN") %>% ggplot(aes(x = Fecha_dx, fill = Ciudad)) + stat_count()
-
-cali <- covid_col %>% filter(Ciudad =="CALI")
-mde <- covid_col %>% filter(Ciudad=="MEDELLIN")
-bog <- covid_col %>% filter(Ciudad=="BOGOTA")
+cali <- covid19_col %>% filter(Ciudad =="CALI")
+mde <- covid19_col %>% filter(Ciudad=="MEDELLIN")
+bog <- covid19_col %>% filter(Ciudad=="BOGOTA")
 
 ggplot(data = bog, aes(x = Fecha_dx, fill = Ciudad)) + stat_count(aes(y=cumsum(..count..))) + stat_count(data = cali, aes(x = Fecha_dx, y=cumsum(..count..))) + stat_count(data = mde, aes(x = Fecha_dx, y=cumsum(..count..))) + scale_x_date(date_labels = "%b %d", date_breaks = "1 day") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + facet_wrap(~Ciudad)
