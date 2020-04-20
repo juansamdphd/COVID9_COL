a0f681fbf1a95f7178dc6655dc961fd2093bb36a
juansamdphd <jucamilo.sanchez@gmail.com>
2020-04-20 15:14:24 -0700

    Update covid19_col.R

--- covid19_col.R
+++ covid19_col.R
@@ -15,7 +15,13 @@ covid19_col <- read.socrata(
   password  = "Sarias23!"
 )
 
-covid19_col$fecha_de_diagn_stico <- date(covid19_col$fecha_de_diagn_stico)
+covid19_col$fecha_de_notificaci_n <- ymd_hms(covid19_col$fecha_de_notificaci_n)
+covid19_col$fecha_de_notificaci_n <- date(covid19_col$fecha_de_notificaci_n)
+
+which(is.na(covid19_col$fecha_de_notificaci_n))
+
+covid19_col$fecha_de_notificaci_n[3653] <- "2020-04-13"
+covid19_col$fecha_de_notificaci_n[3654] <-  "2020-04-18"
 
 covid19_col %>% 
   ggplot(aes(x=fecha_de_diagn_stico, colour = ciudad_de_ubicaci_n)) + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Bogota"), aes(y=cumsum(..count..)), geom = "step") + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Cali"), aes(y=cumsum(..count..)), geom = "step") + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Medellin"), aes(y=cumsum(..count..)), geom = "step") + scale_x_date(date_labels = "%b %d", date_breaks = "1 day") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
