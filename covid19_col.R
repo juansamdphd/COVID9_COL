24d99cb2f62e1aac76635dc5d607ca185d7491ee
juansamdphd <jucamilo.sanchez@gmail.com>
2020-04-20 21:25:17 -0700

    Update covid19_col.R

--- covid19_col.R
+++ covid19_col.R
@@ -1,5 +1,6 @@
 library(RSocrata)
 library(tidyverse)
+library(lubridate)
 
 # Read Data from INS CO
 # key name: covid19_col
@@ -31,8 +32,17 @@ cali_casos_notif <- covid19_col %>%
 covid19_col %>% filter(ciudad_de_ubicaci_n == "Cali") %>%
   count(atenci_n == "Fallecido")
 
+covid19_col %>% filter(atenci_n == "Fallecido") %>% View()
+
 cali_casos_muerte <- covid19_col %>% 
   ggplot(aes(x=fecha_de_muerte)) + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Cali"), aes(y=cumsum(..count..)), binwidth = 1, geom = "step", colour = "red") + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Cali"), aes(y=cumsum(..count..), label = ..count..), binwidth = 1, geom = "text", colour = "black", vjust = -0.25) + scale_x_date(name = "Fecha de Notificacion", date_labels = "%b %d", date_breaks = "1 day", minor_breaks = "1 day") + scale_y_continuous(name = "# de casos", breaks = seq(0,500,50)) + theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "none")
+
+### Combine graphs (in progress)
+covid19_col %>% 
+  ggplot() + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Cali"), aes(x= fecha_de_notificaci_n, y=cumsum(..count..)), binwidth = 1, geom = "step", colour = "red") + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Cali"), aes(x=fecha_de_notificaci_n, y=cumsum(..count..)), binwidth = 1, geom = "point", colour = "red") + stat_bin(data=subset(covid19_col, ciudad_de_ubicaci_n == "Cali"), aes(x=fecha_de_notificaci_n, y=cumsum(..count..), label = cumsum(..count..)), binwidth = 1, geom = "text", colour = "black", vjust = -0.25) + scale_x_date(name = "Fecha de Notificacion", date_labels = "%b %d", date_breaks = "1 day", minor_breaks = "1 day") + scale_y_continuous(name = "# de casos", breaks = seq(0,500,50)) + 
+  theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "none")
+
+
 
 ## Log10 graph
 covid19_col %>% 
